49a59b790744e065f703691d023c8c8c2b87602b
Stephen Forrest <stephen.forrest@onthebeach.co.uk>
2017-10-28 15:50:05 +0100

    add views and routes

--- app/controllers/complaints_controller.rb
+++ app/controllers/complaints_controller.rb
@@ -3,4 +3,27 @@ class ComplaintsController < ApplicationController
   def index
 
   end
+
+  def who
+
+  end
+
+  def when
+
+  end
+
+  def where
+
+  end
+
+  def what
+
+  end
+  
+  def why
+
+  end
+  def trans
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'complaints#index'
+  get '/who', to: "complaints#who"
+  get '/what', to: "complaints#what"
+  get '/where', to: "complaints#where"
+  get '/when', to: "complaints#when"
+  get '/why', to: "complaints#why"
+
+  post '/who', to: "complaints#trans"
+  post '/what', to: "complaints#trans"
+  post '/where', to: "complaints#trans"
+  post '/when', to: "complaints#trans"
+  post '/why', to: "complaints#trans"
 end
