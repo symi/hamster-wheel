7cc6969da4ee9a6b51360c6202f72b8bfffc4bf9
Elizabeth Dalton <you@example.com>
2017-10-28 16:24:26 +0100

    seeds file creation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171028134859) do
+ActiveRecord::Schema.define(version: 20171028150512) do
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "hotels", force: :cascade do |t|
     t.string "name"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10000.times{
+Company.create([{
+  name: Faker::Company.name
+}])
+}
